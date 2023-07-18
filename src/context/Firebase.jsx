10d70f321c8b9b4bb3131a57b84ef1aae2c225fe
Mayank Tripathi <84989130+MAYANK-TRIPATH@users.noreply.github.com>
2023-07-19 00:22:06 +0530

    CRUD added

--- src/context/Firebase.jsx
+++ src/context/Firebase.jsx
@@ -2,6 +2,8 @@ import { createContext, useContext, useState, useEffect } from "react";
 import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword,
    GoogleAuthProvider, signInWithPopup, onAuthStateChanged  } from "firebase/auth";
+import {getFirestore, collection, addDoc} from "firebase/firestore";  
+import { getStorage, ref, uploadBytes } from "firebase/storage"; 
 
 const FirebaseContext = createContext(null);
 
@@ -18,6 +20,9 @@ export const useFirebase = () => useContext(FirebaseContext);
 
 const firebaseApp = initializeApp(firebaseConfig);
 const firebaseAuth = getAuth(firebaseApp);
+const firestore = getFirestore(firebaseApp);
+const storage = getStorage(firebaseApp); 
+
 
 const googleProvider = new GoogleAuthProvider();
 
@@ -38,12 +43,29 @@ export const  FirebaseProvider = (props) => {
    signInWithEmailAndPassword(firebaseAuth, email, password);
 
    const signinWithGoogle = () => signInWithPopup(firebaseAuth, googleProvider)
+   
+   console.log(user);
+
+   const handleCreateNewListing = async (name, isbn, price, cover) => {
+     const imageRef = ref(storage, `uploads/images/${Date.now()}-${cover.name}`)
+     const uploadResult = await uploadBytes(imageRef, cover);
+     return await addDoc(collection(firestore, "books"), {
+      name,
+      isbn,
+      price,
+      imageURL: uploadResult.ref.fullPath,
+      userID: user.uid,
+      userEmail: user.email,
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+     });
+   }; 
 
    const isLoggedIn = user ? true : false;
 
     return (
     <FirebaseContext.Provider value={{signinWithGoogle, signupUserWithEmailAndPassword,
-     signinUserWithEmailAndPass, isLoggedIn}}>
+     signinUserWithEmailAndPass, handleCreateNewListing, isLoggedIn}}>
       {props.children}</FirebaseContext.Provider>
     )
 };
